e2c0cc4f1009f7e36680774a1daa955568be3e8f
Diego <diego.edo.valdes@gmail.com>
2021-05-22 22:04:58 -0400

    actualizado

--- patron3.rb
+++ patron3.rb
@@ -1,11 +1,5 @@
 numero=ARGV[0].to_i
-(numero*2).times do |i|
-    if i%3 == 0
-    print '..'
-    elsif i%3 == 1
-    print '**'
-    else
-    print '||'
-    end
+(numero-1).times do |i|
+    print '..**||'
 end
 print "\n"
